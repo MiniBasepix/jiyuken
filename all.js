671da323854aecd01d98e080c193dbbccd510ec1
Clos Jasl <85016704+MiniBasepix@users.noreply.github.com>
2022-01-16 12:56:25 +0900

    Add files via upload

--- all.js
+++ all.js
@@ -10,3 +10,18 @@ $(function(){
 function stopload(){
   $('.loaded').delay(600).fadeOut(300);
 }
+
+var userAgent = window.navigator.userAgent.toLowerCase();
+
+if(userAgent.indexOf('msie') != -1 ||
+        userAgent.indexOf('trident') != -1) {
+    console.log('Internet Explorer をJiyukenは対応していません。Microsoft Edge またはGoogle Chrome を使用してください。');
+} else if(userAgent.indexOf('safari') != -1) {
+    console.log('SafariをJiyukenは対応していません。Microsoft Edge またはGoogle Chrome を使用してください。');
+} else if(userAgent.indexOf('firefox') != -1) {
+    console.log('FireFoxをJiyukenは対応していません。Microsoft Edge またはGoogle Chrome を使用してください。');
+} else if(userAgent.indexOf('opera') != -1) {
+    console.log('OperaをJiyukenは対応していません。Microsoft Edge またはGoogle Chrome を使用してください。');
+} else {
+    console.log('このブラウザをJiyukenは対応していません。Microsoft Edge またはGoogle Chrome を使用してください。');
+}
